b51da5ac181af24e476f669a268d5514a565ba73
Hrishikesh Kumar Singh <hrishikeshkum.slsn11abcd@gmail.com>
2026-02-16 00:48:42 +0530

    some change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -20,6 +20,7 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 
@@ -29,8 +30,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
 
-app.get('/', (req, res) => {
-    res.send("Backend is running 🚀");
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;
